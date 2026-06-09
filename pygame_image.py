7eb6618881fc0d8c9e2c8aa75b29e753b7e9d9d2
yuna <c0a2509236@edu.teu.ac.jp>
2026-06-09 18:12:55 +0900

    演習①

--- pygame_image.py
+++ pygame_image.py
@@ -18,18 +18,25 @@ def main():
     tmr = 0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
+            if event.type == pg.QUIT: 
+                return
         key_lst = pg.key.get_pressed()
+       
         if key_lst[pg.K_UP]:
             kk_rct.move_ip(0, -1) 
         if key_lst[pg.K_DOWN]:  
             kk_rct.move_ip(0, +1)
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip(-1, 0)
+            
         if key_lst[pg.K_RIGHT]:
             kk_rct.move_ip(+1, 0)
+        
+        kk_rct.move_ip(-1,0)
+        
         for even in pg.event.get():
             if event.type == pg.QUIT:
+                kk_img = pg.transform.flip(kk_img, True, False)
                 return
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])
